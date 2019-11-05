485e176bae63cff8376e542c63decddd85086f90
Luciano Pinedo <lucianopinedo@gmail.com>
2019-11-05 16:46:24 -0400

    Empty proyect interface and basic request

--- web-app-front/src/screens/proyects/index.js
+++ web-app-front/src/screens/proyects/index.js
@@ -2,51 +2,65 @@ import React, {useState} from 'react';
 import ProyectTable from './ProyectTable';
 import AdvancedSearch from '../../components/advancedSearch'
 import {Container, Row, Col, Card, Button, Input, Label, Collapse} from 'reactstrap';
+import { FaPlus } from 'react-icons/fa';
 const Proyects = () => {
     const [newProyect, setNewProyect] = useState(false)
+    const [proyectName, setProyectName] = useState('')
+    const [proyects, setProyects] = useState([])
+    function createProyect(proyectCriteria) {
+        const proyect = {
+          proyectCriteria: proyectCriteria,
+          totalCandidates: 0,
+          proyectName: proyectName,
+          startDate: new Date()
+        }
+        console.log(proyect)
+    }
 
-    function createProyect(proyectParams) {
-        console.log(proyectParams)
-    }
-    function AddProyect()  {
-        return (
-          <Row className="mb-2 mt-2">
-            <Col md={12}>
-                <Label for="nombre"> <b>Nombre del Proyecto </b> </Label>
-                <Input> </Input>
-            </Col>
-            <Col md={12} className="mb-2 mt-2">
-            <b> Condiciones de búsqueda </b>
-            </Col>
-            <AdvancedSearch proyects onSubmit={createProyect}/>
-          </Row>
-        );
-    }
 
 
 
 
     return (
-      <Container className="mb-2" >
+      <Container className="mb-2">
         <Row className="justify-content-center">
           <Col lg={12} className="d-flex flex-column">
-            <Card className="my-lg-5 my-md-4 my-3">
-            <ProyectTable proyects={[{ name: 'hola', id:'123' }]} />
-            </Card>
+            { proyects.length > 0 ?            <Card className="my-lg-5 my-md-4 my-3">
+              <ProyectTable proyects={[]} />
+            </Card> : <Card className=" mt-2 mb-2 d-flex align-items-center"> <h3 className="text-center mb-4"> No existen proyectos</h3>  <h4 className="text-muted text-center"> Presione el botón "Nuevo proyecto" para agregar uno</h4>  </Card>}
           </Col>
         </Row>
         <Row className="justify-content-center">
           <Col lg={12} className="d-flex flex-column">
-              <Card>
-              <Button onClick={() => setNewProyect(!newProyect)} color="success" > Nuevo Proyecto </Button>
-                  <Col className="ml-1 mr-1">
-              <Collapse isOpen={newProyect}>
-                <AddProyect/>
+            <Card>
+              <Button
+                onClick={() => setNewProyect(!newProyect)}
+                color="success"
+              >
+                Nuevo Proyecto
+              </Button>
+              <Col className="ml-1 mr-1">
+                <Collapse isOpen={newProyect}>
+                  <Row className="mb-2 mt-2">
+                    <Col md={12}>
+                      <Label for="nombre">
+                        <b>Nombre del Proyecto </b>
+                      </Label>
+                      <Input
+                        value={proyectName}
+                        onChange={e => setProyectName(e.target.value)}
+                      ></Input>
+                    </Col>
+                    <Col md={12} className="mb-2 mt-2">
+                      <b> Condiciones de búsqueda </b>
+                    </Col>
+                    <AdvancedSearch proyects onSubmit={createProyect} />
+                  </Row>
                 </Collapse>
-                </Col>
-                </Card>
               </Col>
-              </Row>
+            </Card>
+          </Col>
+        </Row>
       </Container>
     );
 };
