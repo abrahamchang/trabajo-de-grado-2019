b6193b12573ae076875b32352360ed47991be9cd
Luciano Pinedo <lucianopinedo@gmail.com>
2020-03-05 23:59:22 -0400

    simple search fixes

--- web-app-front/src/screens/search/index.js
+++ web-app-front/src/screens/search/index.js
@@ -94,8 +94,8 @@ const Search = (props) => {
                 <th> Nombre </th>
                 <th> Apellido </th>
                 <th> Nombre de archivo </th>
-                <th> Puntuación </th>
-                <th> Confianza </th>
+                {/* <th> Puntuación </th>
+                <th> Confianza </th> */}
                 <th></th>
               </tr>
             </thead>
@@ -105,8 +105,8 @@ const Search = (props) => {
                   <td>{searchEntry.metadata.firstName} </td>
                   <td>{searchEntry.metadata.lastName}</td>
                   <td>{searchEntry.metadata.fileName}</td>
-                  <td> {`${searchEntry.result_metadata.score.toFixed(2)} (${searchEntry.result_metadata.score * 100 / maxScore}%)`} </td>
-                  <td> {searchEntry.result_metadata.confidence.toFixed(2)} </td>
+                  {/* <td> {`${searchEntry.result_metadata.score.toFixed(2)} (${searchEntry.result_metadata.score * 100 / maxScore}%)`} </td>
+                  <td> {searchEntry.result_metadata.confidence.toFixed(2)} </td> */}
                   <td>
                     <Button color="primary" onClick={() => props.navigate(searchEntry.id, {state: searchEntry})}> Ver más </Button>
                   </td>
